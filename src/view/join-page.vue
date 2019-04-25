5c44cbb46004af0588f20a3868573e2ef6879bad
zhaoshuqi <852056893@qq.com>
2019-04-25 16:28:27 +0800

    上传后台管理系统

---
+++ src/view/join-page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <TreeSelect></TreeSelect>
+  </div>
+</template>
+
+<script>
+import { TreeSelect } from "@/components/tree-select/tree-select.vue";
+export default {
+  components: { TreeSelect },
+  data() {
+    return {};
+  }
+};
+</script>
